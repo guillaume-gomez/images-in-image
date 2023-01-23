858cb0865f14e72be8643b7717db88edc401ae93
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-01-24 00:04:24 +0100

    usestate instead of useref

--- src/Components/UseImage.tsx
+++ src/Components/UseImage.tsx
@@ -1,37 +1,36 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 
 //process.env.PUBLIC_URL + "logo512.png"
 
 function UseImage(url: string) {
-  const imageRef = useRef<HTMLImageElement>();
+  const [image, setImage] = useState<HTMLImageElement>();
+
   useLayoutEffect(
      () => {
-      const img = document.createElement('img');
+      const img = new Image();
 
       function onload() {
-        imageRef.current = img;
+        setImage(img);
         console.log("loaded " + url)
       }
 
       function onerror() {
-        imageRef.current = undefined;
         console.log("error " + url)
       }
+      img.src = url;
 
       img.addEventListener('load', onload);
       img.addEventListener('error', onerror);
       
-      img.src = url;
 
       return function cleanup() {
         img.removeEventListener('load', onload);
         img.removeEventListener('error', onerror);
       };
     },
-    [url]
+    []
   );
-  console.log("hhh " + imageRef.current)
-  return imageRef.current;
+  return image;
 }
 
 export default UseImage;
