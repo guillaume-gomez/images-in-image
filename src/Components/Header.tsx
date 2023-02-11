a81bdaa4591c699896f021d69478a923ba3b0332
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-02-11 10:46:28 +0100

    create basic component for the header and the footer

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <a className="btn btn-ghost normal-case text-xl">Images in Image</a>
+    </div>
+  );
+}
+
+export default Header;
