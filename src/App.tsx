e38b16b9334a53728d3c175457d22f437e713c52
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-02-13 00:18:09 +0100

    add save button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, RefObject } from 'react';
 import { minBy } from "lodash";
+import { format as formatFns } from "date-fns";
 import useImage from "./Components/UseImage";
 import useFromImageToImages from "./Components/useFromImageToImages";
 import image from "./image.png";
@@ -11,23 +12,16 @@ import './App.css';
 
 
 function App() {
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
+  const [ratio, setRatio] = useState<number>(32);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvasFinal = useRef<HTMLCanvasElement>(null);
+  const anchorRef = useRef<HTMLAnchorElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
   const black = useImage(process.env.PUBLIC_URL + "sprites/test/black.png");
   const white = useImage(process.env.PUBLIC_URL + "sprites/test/white.png");
   const red = useImage(process.env.PUBLIC_URL + "sprites/test/red.png");
   const green = useImage(process.env.PUBLIC_URL + "sprites/test/green.png");
   const blue = useImage(process.env.PUBLIC_URL + "sprites/test/blue.png");
-
-  useEffect(() => {
-    if(imageRef.current) {
-      setWidth(imageRef.current.width * 32);
-      setHeight(imageRef.current.height * 32);
-    }
-  }, [imageRef])
 
   const picturesData = [
     {value: 0, sprite: black},
@@ -42,7 +36,35 @@ function App() {
   function onClick() {
     if(imageRef.current && canvasFinal.current && canvasRef.current) {
       generateImage(imageRef.current, canvasFinal.current);
-      resizeImage(canvasFinal.current, canvasRef.current, imageRef.current.width, imageRef.current.height)
+      resizeImage(canvasFinal.current, canvasRef.current, imageRef.current.width, imageRef.current.height);
+    }
+  }
+
+  function resizeFinalImage(newRatio: number) {
+    if(canvasFinal.current && imageRef.current) {
+      const width = imageRef.current.width * ratio;
+      const height = imageRef.current.height * ratio;
+      canvasFinal.current.width = width;
+      canvasFinal.current.height = height;
+
+      generateImage(imageRef.current, canvasFinal.current);
+      resizeImage(canvasFinal.current, canvasFinal.current, width, height);
+
+      setRatio(newRatio);
+      
+      console.log("je passe la")
+    }
+    //setWidth(width);
+    //setHeight(height);
+  }
+
+  function saveImage() {
+    if(canvasFinal.current && anchorRef.current) {
+      const format = "jpeg";
+      const dataURL = canvasFinal.current.toDataURL(`image/${format}`);
+      const dateString = formatFns(new Date(), "dd-MM-yyyy-hh-mm");
+      (anchorRef.current as any).download = `${dateString}-image-in-images.${format}`;
+      anchorRef.current.href = dataURL.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
     }
   }
 
@@ -50,19 +72,20 @@ function App() {
     <div className="flex flex-col gap-8 items-center">
        <Header />
        <h1 className="text-3xl font-bold underline text-primary">
-      Hello Alice!
-    </h1>
+        Hello Alice!
+       </h1>
         <img src={image} ref={imageRef} />
         <canvas ref={canvasRef} />
-        <input type="number" value={width} onChange={(e) => setWidth(parseInt(e.target.value)) }/>
-        <input type="number" value={height} onChange={(e) => setHeight(parseInt(e.target.value)) }/>
-        <canvas ref={canvasFinal} width={width} height={height}/>
+        <p>{ratio}</p>
+        <input min={1} max={32} type="range" className="range range-primary" value={ratio} onChange={(e) =>  resizeFinalImage(parseInt(e.target.value)) }/>
         <a
           className="App-link"
           onClick={onClick}
         >
-          Ahhhh Click
+          Click pour generer l'image wesh !
         </a>
+        <canvas ref={canvasFinal} style={{ background: "red"}}/>
+         <a ref={anchorRef} className="btn btn-primary" onClick={ () => saveImage()}>Save</a>
         <Footer />
     </div>
   );
