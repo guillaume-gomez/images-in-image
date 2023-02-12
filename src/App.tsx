e8d6a4a944d8d63744d457beb563664d73cf4f54
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-02-12 14:06:17 +0100

    start to design the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { minBy } from "lodash";
-import logo from './logo.svg';
 import useImage from "./Components/UseImage";
 import useFromImageToImages from "./Components/useFromImageToImages";
 import image from "./image.png";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 import './App.css';
 
 
 function App() {
+  const [width, setWidth] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvasFinal = useRef<HTMLCanvasElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
@@ -17,6 +21,13 @@ function App() {
   const red = useImage(process.env.PUBLIC_URL + "sprites/test/red.png");
   const green = useImage(process.env.PUBLIC_URL + "sprites/test/green.png");
   const blue = useImage(process.env.PUBLIC_URL + "sprites/test/blue.png");
+
+  useEffect(() => {
+    if(imageRef.current) {
+      setWidth(imageRef.current.width * 32);
+      setHeight(imageRef.current.height * 32);
+    }
+  }, [imageRef])
 
   const picturesData = [
     {value: 0, sprite: black},
@@ -35,22 +46,24 @@ function App() {
     }
   }
 
-
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="flex flex-col gap-8 items-center">
+       <Header />
+       <h1 className="text-3xl font-bold underline text-primary">
+      Hello Alice!
+    </h1>
         <img src={image} ref={imageRef} />
-        <h2>Guigui</h2>
         <canvas ref={canvasRef} />
-        <h2>Gaga</h2>
-        <canvas ref={canvasFinal} />
+        <input type="number" value={width} onChange={(e) => setWidth(parseInt(e.target.value)) }/>
+        <input type="number" value={height} onChange={(e) => setHeight(parseInt(e.target.value)) }/>
+        <canvas ref={canvasFinal} width={width} height={height}/>
         <a
           className="App-link"
           onClick={onClick}
         >
           Ahhhh Click
         </a>
-      </header>
+        <Footer />
     </div>
   );
 }
