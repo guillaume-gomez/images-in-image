e7531df5513ebedceea3c12b177c663b7c7d13ac
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-02-25 13:11:50 +0100

    design the form

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { format as formatFns } from "date-fns";
 import useImage from "./Hooks/UseImage";
 import useFromImageToImages from "./Hooks/useFromImageToImages";
 import StepFormCard from "./Components/StepFormCard";
-import image from "./image.png";
+import InputFileWithPreview from "./Components/InputFileWithPreview";
+import sampleImage from "./image.png";
 
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
 import './App.css';
 
+type AlgorithmType = "optimized" | "biggestImage";
 
 function App() {
   const [ratio, setRatio] = useState<number>(32);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [algorithmType, setAlgorithmType] = useState<AlgorithmType>("optimized");
+  const [image, setImage] = useState<HTMLImageElement>();
   const canvasFinal = useRef<HTMLCanvasElement>(null);
   const anchorRef = useRef<HTMLAnchorElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
@@ -30,19 +33,29 @@ function App() {
     { color: { red: 255, green: 0, blue: 0 }, sprite: red},
     { color: { red: 255, green: 255, blue: 255 }, sprite: white}
   ];
+  const { generateImage, optimizedGenerateImage } = useFromImageToImages({picturesData, dominantImageSize: 32});
 
-  const { generateImage, optimizedGenerateImage, resizeImage, optimizedResize } = useFromImageToImages({picturesData, dominantImageSize: 32});
-
-  function onClick() {
-    if(imageRef.current && canvasFinal.current && canvasRef.current) {
-      //generateImage(imageRef.current, canvasFinal.current);
-      optimizedGenerateImage(imageRef.current, canvasFinal.current)
-      //resizeImage(canvasFinal.current, canvasRef.current, imageRef.current.width, imageRef.current.height);
+  function moveTo(id: string) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
+
+  function generateImagesInImage() {
+    if(imageRef.current && canvasFinal.current) {
+      if(algorithmType === "optimized") {
+        optimizedGenerateImage(imageRef.current, canvasFinal.current);
+      } else {
+        generateImage(imageRef.current, canvasFinal.current);
+      }
+    }
+    moveTo("result");
+  }
+
   function resizeFinalImage(newRatio: number) {
-    if(canvasFinal.current && imageRef.current) {
+    /*if(canvasFinal.current && imageRef.current) {
       const width = imageRef.current.width * ratio;
       const height = imageRef.current.height * ratio;
       canvasFinal.current.width = width;
@@ -51,7 +64,7 @@ function App() {
       generateImage(imageRef.current, canvasFinal.current);
       setRatio(newRatio);
 
-    }
+    }*/
   }
 
   function saveImage() {
@@ -70,29 +83,56 @@ function App() {
           <div className="container">
             <div className="w-full p-4 text-base-content glass xl:rounded-box flex flex-col gap-4 bg-opacity-60 xl:pb-0">
               <StepFormCard
+                id="choose-image"
                 stepNumber={1}
                 title="Choose an image"
                 nextButtonText="Next 👉"
+                onClick={() => moveTo("custom-palette")}
               >
-                <h1>J'aodre </h1>
+                <InputFileWithPreview onChange={(image) => setImage(image)} value={image} />
+              </StepFormCard>
+              <StepFormCard
+                id="custom-palette"
+                stepNumber={2}
+                title="Custom your palette"
+                nextButtonText="Next 👉"
+                onClick={() => moveTo("algorithm")}
+              >
+                { [black, white, red, green, blue].map(dominantImage => {
+                  return <InputFileWithPreview onChange={(image) => {}} value={dominantImage} />
+                })
+
+                }
+              </StepFormCard>
+              <StepFormCard
+                id="algorithm"
+                stepNumber={3}
+                title="Select the algorithm"
+                nextButtonText="Generate 🚀"
+                onClick={generateImagesInImage}
+              >
+                <select
+                  value={algorithmType}
+                  onChange={(event) => setAlgorithmType(event.target.value as AlgorithmType)}
+                  className="select select-primary w-full max-w-xs">
+                  <option disabled selected >Select the algorithm</option>
+                  <option value="optimized">Optimised</option>
+                  <option value="biggestImage"> Biggest Image</option>
+                </select>
               </StepFormCard>
 
-             <h1 className="text-3xl font-bold underline text-primary">
-              Hello Michel!
-             </h1>
-              <img src={image} ref={imageRef} />
-              <canvas ref={canvasRef} />
+              <StepFormCard
+                id="result"
+                stepNumber={4}
+                title="See the Result"
+              >
+                <canvas className="overflow-auto" ref={canvasFinal} style={{ background: "blue"}}/>
+                <p>Download image could take times if the image is large</p>
+                <a ref={anchorRef} className="btn btn-primary" onClick={ () => saveImage()}>Save</a>
+              </StepFormCard>
+              <img className="hidden" src={sampleImage} ref={imageRef} />
               <p>{ratio}</p>
               <input min={1} max={32} type="range" className="range range-primary" value={ratio} onChange={(e) =>  resizeFinalImage(parseInt(e.target.value)) }/>
-              <button
-                className="btn btn-secondary"
-                onClick={onClick}
-              >
-                Click pour generer l'image wesh !
-              </button>
-              <canvas ref={canvasFinal} style={{ background: "blue"}}/>
-              <p>Download image could take times if the image is large</p>
-              <a ref={anchorRef} className="btn btn-primary" onClick={ () => saveImage()}>Save</a>
             </div>
           </div>
         <Footer />
--- src/Components/StepFormCard.tsx
+++ src/Components/StepFormCard.tsx
@@ -4,20 +4,30 @@ interface StepFormCardProps {
   children: React.ReactNode;
   stepNumber: number;
   title: string;
-  nextButtonText: string;
+  nextButtonText?: string;
+  onClick?: () => void;
+  id: string;
 }
 
-function StepFormCard({children, stepNumber, title, nextButtonText} : StepFormCardProps) {
+function StepFormCard({ id, children, stepNumber, title, nextButtonText, onClick} : StepFormCardProps) {
   return (
-    <div className="card card-side bg-base-100 shadow-xl">
-
-          <div className="badge badge-lg">{stepNumber}</div>
+    <div id={id} className="card card-side bg-base-100 shadow-xl">
+      <div className="mt-2 ml-2">
+        <div className="badge badge-lg">
+          {stepNumber}
+        </div>
+      </div>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         {children}
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">{nextButtonText}</button>
-        </div>
+        {
+          nextButtonText ?
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={onClick}>{nextButtonText}</button>
+          </div> :
+          <></>
+        }
+
       </div>
     </div>
   );
