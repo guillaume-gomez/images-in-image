da6fc9532c79c74ef92ce5caad3ef1a7d2dc02ed
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-10-05 14:17:42 +0200

    update header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="navbar bg-primary text-primary-content">
+    <div className="navbar bg-primary text-primary-content gap-2">
+       <img
+        className="rounded-lg" src={process.env.PUBLIC_URL + '/logo512.png'}
+        style={{border: "1px solid black"}}
+        width="64px"
+      />
       <a className="btn btn-ghost normal-case text-xl">Images in Image</a>
     </div>
   );
